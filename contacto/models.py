e0d51cf9872908ac96d7f27b0b6eff394e02ba1c
Amador <sphi02ac@gmail.com>
2012-12-19 11:21:27 -0400

    Clean up y agregado los links de contacto.

--- contacto/models.py
+++ contacto/models.py
@@ -5,5 +5,5 @@ class Contacto(models.Model):
 	nombres = models.CharField(max_length=100)
 	apellidos = models.CharField(max_length=200)
 	fn = models.DateField('Fecha de nacimiento')
-	email = models.CharField(max_length=100)
+	email = models.EmailField()
 	twitter = models.CharField(max_length=100)
